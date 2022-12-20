b653057169855937af8933cf76e245413d435691
Marcin Bejger <marcin.bejger@gmail.com>
2022-12-20 19:22:37 +0100

    project template prepared and ready to start working

---
+++ my-react-app/src/theme/GlobalStyle.js
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+
+*, *::before , *::after {
+    box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+    monospace;
+}
+.linkcolor{
+  color:white;
+  
+}
+.linkformat{
+    text-decoration:none;
+    margin-right:8px;
+}
+`;
