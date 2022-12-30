62df915dd2cae883d377efb91b6dd6c283dd9bb2
Marcin Bejger <marcin.bejger@gmail.com>
2022-12-30 21:59:08 +0100

    styles modified

--- my-react-app/src/components/AboutUs/AboutUs.style.js
+++ my-react-app/src/components/AboutUs/AboutUs.style.js
@@ -13,3 +13,10 @@ export const StyledParagraph = styled.p`
   font-size: 20px;
   font-weight: bold;
 `;
+
+export const StyledMainDiv = styled.div`
+  background-color: lightblue;
+  /* background: url('../../assets/background2.jpeg'); */
+  overflow: hidden;
+  padding: 50px;
+`;
