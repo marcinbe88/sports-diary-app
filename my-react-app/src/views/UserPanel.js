bda6798fdd5f647b97ef3878402b8fdc685bae6c
Marcin Bejger <marcin.bejger@gmail.com>
2023-01-22 20:22:11 +0100

    setting files

--- my-react-app/src/views/UserPanel.js
+++ my-react-app/src/views/UserPanel.js
@@ -4,6 +4,18 @@ export function UserPage() {
   return (
     <>
       <UserPanel />
+
+      {/* <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<UserPanel />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/userpanel" element={<UserPage />} />
+        </Routes>
+      </BrowserRouter> */}
+
     </>
   );
 }
